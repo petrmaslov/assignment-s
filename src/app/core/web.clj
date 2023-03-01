6f0c2ece1558db94711bcaa18d45675797983ae1
Petr Maslov <petrmaslov@petrmaslov.ru>
2023-03-01 18:41:43 +0300

    Add reading from API by order

--- src/app/core/web.clj
+++ src/app/core/web.clj
@@ -1,10 +1,69 @@
 (ns app.core.web
-  "Freedom to ask. That's why we love the Internet. It is in this namespace where all the magic happens.")
+  "Freedom to ask. That's why we love the Internet. It is in this namespace where all the magic happens."
+  (:require
+    [app.core.config :as config]
+    [cheshire.core :as json]
+    [clj-http.client :as client]
+    [clojure.walk :as walk]))
+
+
+(defn save-get
+  [uri opts]
+  (try
+    (client/get uri opts)
+    (catch Exception e {:uri     uri
+                        :opts    opts
+                        :error   e
+                        :message (:message e)})))
+
+
+(defn read-api
+  "Returns the body of the API response."
+  [api-name tag]
+  (when-let [{:keys [uri timeout tags opts]
+              :or   {timeout 1000}} (get (config/get) api-name)]
+    (-> uri
+      (save-get {:cookie-policy :none
+                 :socket-timeout timeout
+                 :connection-timeout timeout
+                 :accept :json
+                 :query-params (into {tags tag} opts)})
+      :body
+      (json/parse-string))))
+
+
+(defn read-api-in-order
+  "Returns vector of maps, where each map is a response from the API."
+  ([api-name tags]
+   (read-api-in-order api-name tags {}))
+  ([api-name tags {:as opts}]
+   (let [{:keys [max-parallel, delay]
+          :or   {max-parallel 1, delay 0}} (-> (config/get)
+                                             (get api-name)
+                                             (merge opts))
+         queue (partition max-parallel max-parallel nil tags)
+         read-api (partial read-api api-name)]
+     (if (seq queue)
+       (->> queue
+         (map-indexed
+           (fn [idx tags]
+             (do
+               (when (not= idx 0) (Thread/sleep delay))
+               (if (> (count tags) 1)
+                 (pmap read-api tags)
+                 (map read-api tags)))))
+         (flatten))
+       {:error? true
+        :message "Incorrect. Use URI-query like: `.../search?tag=<tag>..."}))))
 
 
 (defn get-statistic-by-tags
   "Returns a map, where the keys are the tags from the questions on the passed topic, and the values are the maps with statistic for answers."
   [api-name tags & {:as opts}]
-  {:api-name api-name
-   :tags tags
-   :opts opts})
+  (let [data (read-api-in-order api-name tags opts)]
+    (if (:error? data)
+      data
+      {:api-name api-name
+       :tags tags
+       :opts opts
+       :data data})))
